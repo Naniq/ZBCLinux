c64202a16fc9b2d7ca73ea795c5c7b55a9f36efe
Naniq <kobberupjensen@gmail.com>
2020-05-29 18:33:15 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,36 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-aptitude -y install expect
-
-// Not required in actual script
-MYSQL_ROOT_PASSWORD=abcd1234
+MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
+MYSQL_ROOT_PASSWORD="secret_password"
 
 SECURE_MYSQL=$(expect -c "
 
 set timeout 10
 spawn mysql_secure_installation
 
-expect \"Enter current password for root (enter for none):\"
-send \"$MYSQL\r\"
+expect "Enter password for user root:"
+send "$MYSQL\r"
 
-expect \"Change the root password?\"
-send \"n\r\"
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "y\r"
 
-expect \"Remove anonymous users?\"
-send \"y\r\"
+expect "New password:"
+send "$MYSQL_ROOT_PASSWORD\r"
 
-expect \"Disallow root login remotely?\"
-send \"y\r\"
+expect "Re-enter new password:"
+send "$MYSQL_ROOT_PASSWORD\r"
 
-expect \"Remove test database and access to it?\"
-send \"y\r\"
+expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
+send "y\r"
 
-expect \"Reload privilege tables now?\"
-send \"y\r\"
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
 
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
 expect eof
 ")
 
 echo "$SECURE_MYSQL"
-
-aptitude -y purge expect
