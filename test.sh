42a8d60dc9ef6015e15dc505d8c427c070d6d1b0
Naniq <kobberupjensen@gmail.com>
2020-05-29 18:36:45 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,40 +1,8 @@
-#!/usr/bin/env bash
-
-MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
-MYSQL_ROOT_PASSWORD="secret_password"
-
-SECURE_MYSQL=$(expect -c "
-
-set timeout 10
-spawn mysql_secure_installation
-
-expect "Enter password for user root:"
-send "$MYSQL\r"
-
-expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "New password:"
-send "$MYSQL_ROOT_PASSWORD\r"
-
-expect "Re-enter new password:"
-send "$MYSQL_ROOT_PASSWORD\r"
-
-expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-expect eof
-")
-
-echo "$SECURE_MYSQL"
+myql --user=root <<_EOF_
+UPDATE mysql.user SET Password='Kode1234!' WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+_EOF_
