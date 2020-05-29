cafbd186bbfcb8d3747c2a95b1c99e1bd5941f24
Naniq <kobberupjensen@gmail.com>
2020-05-29 12:23:41 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#Update kernal
+yum update -y
+yum upgrade -y
+#Install required packages
+yum install
+
+#Install php
+yum install epel-release -y
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+yum install yum-utils -y
+yum-config-manager --enable remi-php73
+yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-pear -y
+
+#Enable apache
+systemctl enable httpd
+systemctl start httpd
+
+#Install mariaDB
+cat > /etc/yum.repos.d/MariaDB.repo <<"EOF"
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.2/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+EOF
+
+sleep 1
+yum install MariaDB-server MariaDB-client -y
+
+#update firewall
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=https
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --reload
+
+#install bind
+yum install bind bind-utils -y
+systemctl enable named
+systemctl start named
+
+#install webmin
+cat > /etc/yum.repos.d/webmin.repo <<"EOF"
+[Webmin]
+name=Webmin Distribution Neutral
+#baseurl=http://download.webmin.com/download/yum
+mirrorlist=http://download.webmin.com/download/yum/mirrorlist
+enabled=1
+EOF
+
+wget http://www.webmin.com/jcameron-key.asc
+
+rpm --import jcameron-key.asc
+yum install webmin
+
+#install openssl
