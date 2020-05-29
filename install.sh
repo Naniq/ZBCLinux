1449e5180241af4afa0757811fc5da18a42c11b6
Naniq <kobberupjensen@gmail.com>
2020-05-29 13:38:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-service=https
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --permanent --add-port=10000/tcp
 firewall-cmd --reload
 
 #install bind
