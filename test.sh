52bfc97d8cb67af1145d9936d39c12185e36fc42
Patrick Kobberup-jensen <kobberupjensen@gmail.com>
2020-06-03 11:05:00 +0200

    yo

--- test.sh
+++ test.sh
@@ -1,8 +1,2 @@
-myql --user=root <<_EOF_
-UPDATE mysql.user SET Password='Kode1234!' WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-_EOF_
+cp ~/ZBCLinux/.my.cnf ~/
+mysql -u root
